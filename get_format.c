1bb5a5a60c26b37df8b93ea4aa1a39b93df8ca6c
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-01 14:46:54 -0700

    adding return for structure file

--- get_format.c
+++ get_format.c
@@ -5,18 +5,22 @@
 void (*get_format(char specifier))(va_list)
 {
 
-    int i = 0;
-    format_t formats = {
-        {'s', print_string},
-        {'c', print_char},
-        {'d', print_digit},
-        {'i', print_digit},
-        {NULL, NULL}
-    };
+	int i = 0;
+	format_t formats[] = {
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_digit},
+		{'i', print_digit},
+		{NULL, NULL}
+	};
 
-    while (formats[i].spec != NULL)
-    {
-        if (specifier == formats[i].spec)
-            
-    }
+	while (formats[i].spec != NULL)
+	{
+		if (specifier == formats[i].spec)
+		{
+			return (formats[i].print_func);
+		}
+
+		i++;
+	}
 }
