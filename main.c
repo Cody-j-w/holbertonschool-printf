29c02c9269fb60ed6ba9b8467a2d784eae4edb28
Cody <codywalenciak@gmail.com>
2023-11-02 12:55:12 -0500

    MAIN.C DOES NOT LEAVE TEST BRANCH

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * DO NOT PUSH TO MAIN
+ * DO NOT PUSH TO MAIN
+ * DO NOT PUSH TO MAIN
+*/
+
+int main(void)
+{
+    _printf("This is %c %s of the %d format specifiers", 'a', "test", 3);
+
+    return (0);
+}
