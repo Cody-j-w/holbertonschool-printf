5c49ae7d90cac8e70f1205204876b8725a917d22
Cody <codywalenciak@gmail.com>
2023-11-03 14:34:26 -0500

    Add print_nothing function

--- formats.c
+++ formats.c
@@ -48,11 +48,25 @@ int print_digit(va_list args)
 
 /**
  * print_mod - prints a percent sign in the case of a double-%
- * @args: 
+ * @args: unused
+ * 
+ * Return: Number of characters printed (should be 1)
 */
 
 int print_mod(__attribute__((unused)) va_list args)
 {
 	putchar('%');
 	return (1);
+}
+
+/**
+ * print_nothing - prints absolutely nothing, exists to catch edge cases
+ * @args: unused
+ * 
+ * Return: 0
+*/
+
+int print_nothing(__attribute__((unused)) va_list args)
+{
+	return (0);
 }
