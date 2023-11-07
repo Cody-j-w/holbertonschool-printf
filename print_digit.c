b06c85bde3c53abb889ab5809ad4686bf05ca5b9
Cody <codywalenciak@gmail.com>
2023-11-06 22:41:01 -0600

    Minor layout change to print_digit

--- print_digit.c
+++ print_digit.c
@@ -10,40 +10,39 @@
  * Return: Number of characters printed
  */
 
-int print_digit(va_list args) {
+int print_digit(va_list args)
+{
 	int count = 0;
 	int abs_calc, abs_hold, track_dig;
 	int value = va_arg(args, int);
 	
-	if (value == INT_MIN) {	
-		putchar ('-');
-		count++;
-		abs_calc = INT_MAX;
-	}
-	else if (value < 0) {
+	abs_calc = value;
+	if (value < 0)
+	{
 		putchar('-');
 		count++;
-		abs_calc = value * -1;
-	}
-	else {
-		abs_calc = value;
+		if (value == INT_MIN)
+			abs_calc = INT_MAX;
+		else
+			abs_calc = value * -1;
 	}
 	abs_hold = abs_calc;
 	track_dig = 1;
-
-	while (abs_hold > 9) {
+	while (abs_hold > 9)
+	{
 		abs_hold = abs_hold / 10;
 		track_dig = track_dig * 10;
 	}
-	while (track_dig >= 1) {
+	while (track_dig >= 1)
+	{
+		count++;
 		if (value == INT_MIN && track_dig == 1)
 		{
 			putchar('8');
-			count++;
 			track_dig = track_dig / 10;
 		}
-		else{
-			count++;
+		else
+		{
 			putchar(((abs_calc / track_dig) % 10) + '0');
 			track_dig = track_dig / 10;
 		}
